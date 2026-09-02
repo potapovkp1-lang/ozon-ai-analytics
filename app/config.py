1f677e7acc11d06e47ab9b6419a14ecb510fa7d0
potapovkp1-lang <potapov.kp1@gmail.com>
2026-09-02 17:22:39 +0300

    Add app/config.py

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    ozon_client_id: str = ""
+    ozon_api_key: str = ""
+    ozon_performance_client_id: str = ""
+    ozon_performance_client_secret: str = ""
+    gpt_action_token: str = ""
+    sync_enabled: bool = False
+    timezone: str = "Europe/Moscow"
+
+
+settings = Settings()
